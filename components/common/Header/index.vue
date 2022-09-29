c1c1ff56703f0a64ec3c6ad1c454c7c8fa8c94e4
陳嘉旭 <73395592+BlaxBerry@users.noreply.github.com>
2022-09-29 19:51:42 +0900

    style: modify header

--- components/common/Header/index.vue
+++ components/common/Header/index.vue
@@ -1,9 +1,9 @@
 <template>
   <el-row>
-    <el-col :span="24" class="hidden-sm-and-down">
+    <el-col :span="24" class="hidden-sm-and-down my-layout-header-pc">
       <CommonHeaderPC />
     </el-col>
-    <el-col :span="24" class="hidden-md-and-up">
+    <el-col :span="24" class="hidden-md-and-up my-layout-header-mobile">
       <CommonHeaderMobile />
     </el-col>
   </el-row>
